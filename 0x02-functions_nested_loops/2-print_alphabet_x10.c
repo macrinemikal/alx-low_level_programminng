f02b1d833f9456756560cfaf83dae7b538e9cf59
macrine mikal <macrinemikal@gmail.com>
2023-04-26 08:29:00 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the alphabet in lowercase.
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void);
+{
+	char kk;
+	int n;
+
+	n=0;
+
+	while (n<10)
+	{
+		kk='a';
+		while (kk<='z')
+		{
+			_putchar(kk);
+			kk++;
+		}
+		-putchar('\n');
+		n++;
+	}
+}
