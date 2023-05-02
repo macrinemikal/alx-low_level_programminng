6e48acc133656c295d11f2dd8ea3e389a94d4b69
macrine mikal <macrinemikal@gmail.com>
2023-05-02 08:35:55 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - a function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * @n: input
+ * @m: input
  * Return: m
  */
 void reset_to_98(int *m)
