3a504455a9b90cf42454049b664f0f97ac314d9f
macrine mikal <macrinemikal@gmail.com>
2023-05-02 08:59:59 -0700

    5. A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#inlude "main.h"
+/**
+ * print_rev -  prints a string, in reverse, followed by a new line.
+ * @s: string to be reversed.
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int mikii = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		miki++;
+		s++;
+	}
+	s--;
+	for (o = miki; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
