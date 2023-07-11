dafd8728fc242e193b0f5e0999aff979fe1225a3
macrinemikal <macrinemikal@gmail.com>
2023-07-11 08:42:03 -0700

    n

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,33 @@
-#include "main.h"
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
+#include "main.h"
 /**
- * read_textfile- Read text file& print to STDOUT.
- * @filename: text file being readd
- * @letters: number of letters to be read
- * @main: execution begins
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * read_textfile - reads a TXT file and prints it to the POSIX standard output
+ * @filename: file 2 be printed
+ * @letters:  letters is the no of letters it should read and print
+ * Return: actual number of letters it could read and print
+ * or if filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes, return 0
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
+	char *buffer;
+	int fd;
 	ssize_t w;
-	ssize_t t;
+	ssize_t r;
 
+	if (filename == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
-
-	free(buf);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	r = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+	free(buffer);
 	close(fd);
 	return (w);
 }
